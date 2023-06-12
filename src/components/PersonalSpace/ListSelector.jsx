ad23e1ff6bb94f432b04438dd9e9bd93e1fbec60
Roman <romalupan89@gmail.com>
2023-06-12 12:04:00 +0300

    add games to local storage

--- src/components/PersonalSpace/ListSelector.jsx
+++ src/components/PersonalSpace/ListSelector.jsx
@@ -1,24 +1,50 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import gameList from '../../../data/gameList';
 import { useStateContext } from '../../../context/StateContext';
 import { useState } from 'react';
 
+// const fromLocal = JSON.parse(localStorage.getItem('games') || '[]');
+
 const ListSelector = ({ OpenListSelector, game }) => {
   const [listOfGames, setListOfGames] = useState([]);
 
-  const addToList = () => {
-    console.log(game);
-    OpenListSelector();
-    setListOfGames([...listOfGames, game]);
+  useEffect(() => {
+    const fromLocal = JSON.parse(localStorage.getItem('games'));
+    if (fromLocal) {
+      setListOfGames(fromLocal);
+    }
+  }, [game]);
+
+  // const updateState = () => {
+  //   setListOfGames([...listOfGames, { ...game }]);
+  // };
+
+  useEffect(() => {
+    if (listOfGames == '[]') {
+      return;
+    } else if (listOfGames.length > 0) {
+      localStorage.setItem('games', JSON.stringify(listOfGames));
+    }
     console.log(listOfGames);
+  }, [listOfGames]);
+
+  const storageHandler = () => {
+    OpenListSelector();
   };
+
   return (
     <>
       <div className="listSelector-component flex flex-col gap-2">
         {gameList.map((list) => (
           <h4
             className="text-lg cursor-pointer active:text-cta"
-            onClick={addToList}
+            onClick={() => {
+              setListOfGames([
+                ...listOfGames,
+                { listType: list.title, ...game },
+              ]);
+              storageHandler();
+            }}
           >
             {list.title}
           </h4>
--- src/components/PersonalSpace/ListView.jsx
+++ src/components/PersonalSpace/ListView.jsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 import GameCard from '../GameCard';
 import { useStateContext } from '../../../context/StateContext';
 
+const fromLocal = JSON.parse(localStorage.getItem('games') || '[]');
+
 const ListView = () => {
+  // !! Here get data from local storage and make changes with it
+
+  const [listOfGames, setListOfGames] = useState(fromLocal);
   return (
     <>
       <div className="listView-component">
-        {/* {listOfGames.map((game) => (
-          <>
-            <h4>{game.name}</h4>
-          </>
-        ))} */}
+        {listOfGames.map((game) => (
+          <Link to={`/game/${game.slug}`} key={game.id}>
+            <div className=" game-container cursor-pointer pb-5 relative">
+              <LazyLoadImage
+                threshold={100}
+                src={game.background_image}
+                alt="game thumbnail image"
+                className="w-game-pic h-36 md:w-36 lg:w-44 md:h-52 lg:h-60 rounded-lg  object-cover"
+              />
+              <div className="absolute">
+                <h4 className="text-white font-medium pl-2 pt-2 pb-2 ">
+                  {game.name}
+                </h4>
+              </div>
+            </div>
+          </Link>
+        ))}
       </div>
     </>
   );
